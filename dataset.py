11f7079fad0ab5b0fa2a8bf0fda0c42b5ca683f8
Phse-coder <2368732089@qq.com>
2022-10-23 22:06:05 +0800

    dataset process

--- dataset.py
+++ dataset.py
@@ -11,8 +11,10 @@ class MyDataset(Dataset):
         with open(filename, "r") as f:
             for line in f:
                 line = line.strip()
-                text, label_text = line.split("***")[0:2]
-                label_tuplelist = [tuple(_.rsplit("=", maxsplit=1)) for _ in label_text.split()]
+                text, label = line.split("***")[0:2]
+                text_tokens, text_labels = text.split(), label.split()
+                assert len(text_tokens) == len(text_labels)
+                label_tuplelist = [(text_tokens[i], text_labels[i]) for i in range(len(text_tokens))]
                 label_tuplelist_idx, inner_offset = 0, 0
                 res = tokenizer(text, padding=PaddingStrategy.MAX_LENGTH, truncation=True)
                 input_ids = res.input_ids
@@ -23,11 +25,11 @@ class MyDataset(Dataset):
                     else:
                         label_tuple = label_tuplelist[label_tuplelist_idx]
                         if token.startswith("##"):
-                            tag = f'I{labels[-1][1:]}' if labels[-1] != "O" else 'O'
+                            tag = f'I{labels[-1][1:]}' if labels[-1] not in ["O", "SPECIAL_TOKEN"] else 'O'
                         else:
                             tag = label_tuple[-1]
                             if tag != 'O':
-                                tag = f'B{tag[1:]}' if labels[-1] == "O" else f'I{tag[1:]}'
+                                tag = f'B{tag[1:]}' if labels[-1] in ["O", "SPECIAL_TOKEN"] else f'I{tag[1:]}'
                         inner_offset += len(token.replace("##", ""))
                         if inner_offset == len(label_tuple[0]):
                             label_tuplelist_idx += 1
