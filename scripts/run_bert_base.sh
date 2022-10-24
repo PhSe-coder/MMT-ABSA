aff5e78315fc217497b69908b9ff02d2972997db
Phse-coder <2368732089@qq.com>
2022-10-24 10:35:14 +0800

    scripts

---
+++ scripts/run_bert_base.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+domains=('rest' 'service' 'laptop' 'device')
+
+export CUDA_VISIBLE_DEVICES=0,1
+output='./out/bert_base/'
+for tar_domain in ${domains[@]};
+do
+    for src_domain in  ${domains[@]};
+    do
+        if [ $src_domain != $tar_domain ];
+        then
+            if [ $src_domain == 'laptop' -a  $tar_domain == 'device' ];
+            then
+                continue
+            fi
+            if [ $src_domain == 'device' -a  $tar_domain == 'laptop' ];
+            then
+                continue
+            fi
+	        python run.py
+                --output_dir "${output}${src_domain}-${tar_domain}"  \
+                --train_file "./data/${src_domain}.train.txt" \
+                --validation_file, "./dataset/${tar_domain}.validation.txt" \
+                --test_file, "./dataset/${tar_domain}.test.txt" \
+                --do_train \
+                --do_eval \
+                --do_predict \
+                --device "cuda:0" \
+                --optimizer "bertAdam",
+                --lr 3e-5 \
+                --batch_size 16 \
+                --num_train_epochs 3
+        fi
+    done
+done
