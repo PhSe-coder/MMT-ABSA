9b4935d97dd60fd6b332707f5248ea8c580c2fa5
Phse-coder <2368732089@qq.com>
2022-10-25 09:26:09 +0800

    model structure

--- model.py
+++ model.py
@@ -6,12 +6,12 @@ class Model(nn.Module):
         super().__init__()
         self.bert = BertModel.from_pretrained(pretrained_model)
         self.config = BertConfig.from_pretrained(pretrained_model)
-        self.dropout = nn.Dropout(0.1)
+        self.dropout = nn.Dropout(self.config.hidden_dropout_prob)
         self.classifier = nn.Linear(self.config.hidden_size, 7)
 
     
-    def forward(self, batch):
-        outputs = self.bert(**batch)
+    def forward(self, **batch):
+        outputs = self.bert(batch)
         sequence_output = outputs[0]
 
         sequence_output = self.dropout(sequence_output)
