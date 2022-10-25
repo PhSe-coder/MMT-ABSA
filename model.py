beadf78e1cb67fc44fff1fa1649e408cf12e1b89
Phse-coder <2368732089@qq.com>
2022-10-25 16:59:41 +0800

    model structure

--- model.py
+++ model.py
@@ -1,19 +1,27 @@
 import torch.nn as nn
-from transformers import BertModel, BertConfig
+from pytorch_transformers import BertModel, BertPreTrainedModel
+from transformers.modeling_outputs import TokenClassifierOutput
 
-class Model(nn.Module):
-    def __init__(self, pretrained_model: str):
-        super().__init__()
-        self.bert = BertModel.from_pretrained(pretrained_model)
-        self.config = BertConfig.from_pretrained(pretrained_model)
-        self.dropout = nn.Dropout(self.config.hidden_dropout_prob)
-        self.classifier = nn.Linear(self.config.hidden_size, 7)
 
-    
-    def forward(self, **batch):
-        outputs = self.bert(batch)
+class BertForTokenClassification(BertPreTrainedModel):
+    def __init__(self, config):
+        super(BertForTokenClassification, self).__init__(config)
+        self.num_labels = config.num_labels
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+
+    def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None):
+        outputs = self.bert(input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask)
         sequence_output = outputs[0]
 
         sequence_output = self.dropout(sequence_output)
-        logits = self.classifier(sequence_output)
-        return logits
+        logits = self.classifier(sequence_output)  # attention_mask size (batch, seq_len)
+        loss = None
+        if labels is not None:
+            loss_fct = nn.CrossEntropyLoss(ignore_index=-1)
+            loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+        return TokenClassifierOutput(
+            loss=loss,
+            logits=logits
+        )
