c9931cdd6cf9af8ee39c9f4c6acf287b6d1ffb4d
Phse-coder <2368732089@qq.com>
2022-10-30 22:39:08 +0800

    models

--- model.py
+++ model.py
@@ -11,17 +11,12 @@ class BertForTokenClassification(BertPreTrainedModel):
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.classifier = nn.Linear(config.hidden_size, config.num_labels)
 
-    def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None):
+    def forward(self, input_ids, token_type_ids=None, attention_mask=None):
         outputs = self.bert(input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask)
         sequence_output = outputs[0]
 
         sequence_output = self.dropout(sequence_output)
         logits = self.classifier(sequence_output)  # attention_mask size (batch, seq_len)
-        loss = None
-        if labels is not None:
-            loss_fct = nn.CrossEntropyLoss(ignore_index=-1)
-            loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
         return TokenClassifierOutput(
-            loss=loss,
             logits=logits
         )
