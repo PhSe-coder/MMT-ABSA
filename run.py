611cc4484892280da6e86f1f5948a413fbf1f88a
Phse-coder <2368732089@qq.com>
2022-11-16 19:00:40 +0800

    model running

--- run.py
+++ run.py
@@ -1,29 +1,33 @@
+import logging
 import math
+import os
+import shutil
 import sys
 from time import localtime, strftime
-import shutil
-from typing import Callable, List
-from eval import absa_evaluate
+from typing import Callable, List, Union
+
 import torch
 import torch.nn as nn
-from torch import Tensor
-from torch.utils.data import DataLoader
-from torch.optim.optimizer import Optimizer
-from optimization import BertAdam
-import os
-import logging
-from tqdm import tqdm
-from transformers import HfArgumentParser, BertTokenizer, set_seed
 from pytorch_transformers import BertModel
+from torch import Tensor
+from torch.optim.optimizer import Optimizer
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from transformers import BertTokenizer, HfArgumentParser, set_seed
+from transformers.modeling_outputs import TokenClassifierOutput
+
 from args import ModelArguments
-from dataset import MyDataset
-from model import BertForTokenClassification
-from constants import TAGS
+from constants import SUPPORTED_MODELS, TAGS
+from dataset import BaseDataset, MMTDataset
+from eval import absa_evaluate
+from model import *
+from optimization import BertAdam
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 stdout_handler = logging.StreamHandler(sys.stdout)
 logger.addHandler(stdout_handler)
+
 
 def parse_args():
     parser = HfArgumentParser(ModelArguments)
@@ -35,6 +39,7 @@ def parse_args():
         args = parser.parse_args_into_dataclasses()
 
     return args
+
 
 def init(args: ModelArguments):
     set_seed(args.seed)
@@ -59,33 +64,48 @@ def init(args: ModelArguments):
     }
     args.optimizer = optimizers[args.optimizer]
     args.initializer = initializers[args.initializer]
-
-    dataset_file = {
-        "train": args.train_file,
-        "validation": args.validation_file,
-        "test": args.test_file
-    }
-    args.dataset_file = dataset_file
     os.makedirs("logs", exist_ok=True)
     log_file = './logs/{}-{}.log'.format(args.model_name, strftime("%y%m%d-%H%M", localtime()))
     logger.addHandler(logging.FileHandler(log_file))
 
+
 class Constructor:
+
+    def model_init(self):
+        model_name = self.args.model_name
+        assert model_name in SUPPORTED_MODELS, f'Model {model_name} is not supported'
+        if model_name == 'bert':
+            model = BertForTokenClassification.from_pretrained(args.pretrained_model,
+                                                               num_labels=self.num_labels)
+        elif model_name == 'mmt':
+            model_1 = BertForTokenClassification.from_pretrained(args.pretrained_model,
+                                                                 num_labels=self.num_labels)
+            model_ema_1 = BertForTokenClassification.from_pretrained(args.pretrained_model,
+                                                                     num_labels=self.num_labels)
+            model_2 = BertForTokenClassification.from_pretrained(args.pretrained_model,
+                                                                 num_labels=self.num_labels)
+            model_ema_2 = BertForTokenClassification.from_pretrained(args.pretrained_model,
+                                                                     num_labels=self.num_labels)
+            model = MMTModel(model_1, model_ema_1, model_2, model_ema_2)
+        self.model: Union[BertForTokenClassification, MMTModel] = model
+
+    def dataset_init(self):
+        args = self.args
+        dataset = MMTDataset if args.model_name == 'mmt' else BaseDataset
+        if args.do_train:
+            self.train_set = dataset(self.args.train_file, self.tokenizer, args.device)
+        if args.do_eval:
+            self.validation_set = dataset(self.args.validation_file, self.tokenizer, args.device)
+        if args.do_predict:
+            self.test_set = dataset(self.args.test_file, self.tokenizer, args.device)
+
     def __init__(self, args: ModelArguments):
         self.args = args
         self.num_labels = len(TAGS)
-        tokenizer = BertTokenizer.from_pretrained(args.pretrained_model, 
-            model_max_length=args.max_seq_length)
-        self.tokenizer = tokenizer
-        files = args.dataset_file['train'], args.dataset_file['validation'], args.dataset_file['test']
-        if args.do_train:
-            self.train_set = MyDataset(files[0], tokenizer, args.device)
-        if args.do_eval:
-            self.validation_set = MyDataset(files[1], tokenizer, args.device)
-        if args.do_predict:
-            self.test_set = MyDataset(files[2], tokenizer, args.device)
-        self.model = BertForTokenClassification.from_pretrained(args.pretrained_model, num_labels=self.num_labels)
-        # self.model = Model(args.pretrained_model)
+        self.tokenizer = BertTokenizer.from_pretrained(args.pretrained_model,
+                                                       model_max_length=args.max_seq_length)
+        self.dataset_init()
+        self.model_init(args.model_name)
         self.model.to(args.device)
         self.print_args()
 
@@ -97,14 +117,17 @@ class Constructor:
                 n_trainable_params += n_params
             else:
                 n_nontrainable_params += n_params
-        logger.info('> n_trainable_params: {0}, n_nontrainable_params: {1}'
-                        .format(n_trainable_params, n_nontrainable_params))
+        logger.info('> n_trainable_params: {0}, n_nontrainable_params: {1}'.format(
+            n_trainable_params, n_nontrainable_params))
         logger.info('> training arguments:')
         for arg in vars(self.args):
             logger.info('>>> {0}: {1}'.format(arg, getattr(self.args, arg)))
 
-    def train(self, loss_func: Callable, optimizer: Optimizer, train_data_loader: DataLoader, 
-            val_data_loader: DataLoader=None):
+    def train(self,
+              loss_func: Callable,
+              optimizer: Optimizer,
+              train_data_loader: DataLoader,
+              val_data_loader: DataLoader = None):
         global_step = 0
         n_total, loss_total = 0, 0
         max_val_f1, max_val_epoch = 0, 0
@@ -113,22 +136,24 @@ class Constructor:
         for epoch in range(int(self.args.num_train_epochs)):
             logger.info('>' * 100)
             logger.info('> epoch: {}'.format(epoch))
-            for i, batch in tqdm(enumerate(train_data_loader), f'epoch: {epoch}', len(train_data_loader)):
+            for i, batch in tqdm(enumerate(train_data_loader), f'epoch: {epoch}',
+                                 len(train_data_loader)):
                 global_step += 1
                 optimizer.zero_grad()
-                targets = batch.pop("labels")
-                outputs = self.model(**batch)
-                outputs = outputs.logits
-                loss = loss_func(outputs.view(-1, self.num_labels), targets.view(-1))
+                targets = batch.pop("gold_labels")
+                outputs: TokenClassifierOutput = self.model(**batch)
+                logits = outputs.logits
+                loss = loss_func(logits.view(-1, self.num_labels), targets.view(-1))
                 loss.backward()
                 optimizer.step()
-                pred_list, gold_list = self.id2label(outputs.argmax(dim=-1).tolist(), targets.tolist())
+                pred_list, gold_list = self.id2label(
+                    logits.argmax(dim=-1).tolist(), targets.tolist())
                 pred_Y.extend(pred_list)
                 gold_Y.extend(gold_list)
-                l = len(outputs)
+                l = len(logits)
                 n_total += l
                 loss_total += loss.item() * l
-                if global_step % self.args.logging_steps == 0 or i == len(train_data_loader) -1:
+                if global_step % self.args.logging_steps == 0 or i == len(train_data_loader) - 1:
                     p, r, f1 = absa_evaluate(pred_Y, gold_Y)
                     inf = {
                         "steps": global_step,
@@ -143,38 +168,42 @@ class Constructor:
                     gold_Y, pred_Y = [], []
             if self.args.do_eval:
                 val_pre, val_rec, val_f1 = self.evaluate(val_data_loader)
-                logger.info(f'> val_pre: {val_pre:.4f}, val_rec: {val_rec:.4f}, val_f1: {val_f1:.4f}')
+                logger.info(
+                    f'> val_pre: {val_pre:.4f}, val_rec: {val_rec:.4f}, val_f1: {val_f1:.4f}')
 
                 if val_f1 > max_val_f1:
                     max_val_f1 = val_f1
                     max_val_epoch = epoch
                     os.makedirs('state_dict', exist_ok=True)
                     path = 'state_dict/{}_{}_{}_val_pre_{}_val_rec_{}_val_f1_{}.pt'.format(
-                        self.args.model_name, self.args.train_file.split("/")[-1].split(".")[0], 
-                        self.args.validation_file.split("/")[-1].split(".")[0], 
-                        round(val_pre, 4), round(val_rec, 4), round(val_f1, 4))
+                        self.args.model_name,
+                        self.args.train_file.split("/")[-1].split(".")[0],
+                        self.args.validation_file.split("/")[-1].split(".")[0], round(val_pre, 4),
+                        round(val_rec, 4), round(val_f1, 4))
                     torch.save(self.model.state_dict(), path)
                     logger.info('>> saved: {}'.format(path))
-            
+
                 if epoch - max_val_epoch >= self.args.patience:
                     logger.info(f'>> early stopping at epoch: {epoch}')
                     break
             else:
                 path = 'state_dict/{}_{}_{}.pt'.format(
-                        self.args.model_name, self.args.train_file.split("/")[-1].split(".")[0],
-                        self.args.test_file.split("/")[-1].split(".")[0])
+                    self.args.model_name,
+                    self.args.train_file.split("/")[-1].split(".")[0],
+                    self.args.test_file.split("/")[-1].split(".")[0])
                 torch.save(self.model.state_dict(), path)
         return path
-    
+
     def evaluate(self, data_loader: DataLoader, to_file=False):
         self.model.eval()
         text, gold_Y, pred_Y = [], [], []
         for _, batch in enumerate(data_loader):
-            targets = batch.pop("labels")
+            targets = batch.pop("gold_labels")
             with torch.no_grad():
                 outputs = self.model(**batch)
                 outputs = outputs.logits
-            pred_list, gold_list = self.id2label(outputs.detach().argmax(dim=-1).tolist(), targets.tolist())
+            pred_list, gold_list = self.id2label(outputs.detach().argmax(dim=-1).tolist(),
+                                                 targets.tolist())
             pred_Y.extend(pred_list)
             gold_Y.extend(gold_list)
             if to_file:
@@ -185,7 +214,7 @@ class Constructor:
                 for i in range(len(gold_Y)):
                     f.write(f"{text[i]}***{pred_Y[i]}***{gold_Y[i]}")
         return absa_evaluate(pred_Y, gold_Y)
-    
+
     def id2label(self, predict: List[List[int]], gold: List[List[int]]):
         gold_Y: List[List[str]] = []
         pred_Y: List[List[str]] = []
@@ -196,7 +225,7 @@ class Constructor:
             gold_Y.append(gold_list)
             pred_Y.append(pred_list)
         return pred_Y, gold_Y
-    
+
     def reset_params(self):
         for child in self.model.children():
             if type(child) != BertModel:  # skip bert params
@@ -215,43 +244,54 @@ class Constructor:
         os.makedirs(self.args.output_dir, exist_ok=True)
         best_model_path = self.args.best_model_path
         if self.args.do_train:
-            train_data_loader = DataLoader(dataset=self.train_set, batch_size=self.args.batch_size, shuffle=True)
-            param_optimizer = [(k, v) for k, v in self.model.named_parameters() if v.requires_grad == True]
+            train_data_loader = DataLoader(dataset=self.train_set,
+                                           batch_size=self.args.batch_size,
+                                           shuffle=True)
+            param_optimizer = [(k, v) for k, v in self.model.named_parameters()
+                               if v.requires_grad == True]
             param_optimizer = [n for n in param_optimizer if 'pooler' not in n[0]]
             no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
-            optimizer_grouped_parameters = [
-                {
-                    'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 
-                    'weight_decay': self.args.l2reg
-                },
-                {
-                    'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 
-                    'weight_decay': 0.0
-                }
-            ]
+            optimizer_grouped_parameters = [{
+                'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+                'weight_decay':
+                self.args.l2reg
+            }, {
+                'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+                'weight_decay':
+                0.0
+            }]
             if self.args.optimizer == BertAdam:
                 optimizer = BertAdam(optimizer_grouped_parameters,
-                            lr=self.args.lr,
-                            warmup=self.args.warmup,
-                            t_total=self.args.num_train_epochs*len(train_data_loader))
+                                     lr=self.args.lr,
+                                     warmup=self.args.warmup,
+                                     t_total=self.args.num_train_epochs * len(train_data_loader))
             else:
                 optimizer = self.args.optimizer(optimizer_grouped_parameters, lr=self.args.lr)
             # self.reset_params()
             val_data_loader = None
             if self.args.do_eval:
-                val_data_loader = DataLoader(dataset=self.validation_set, batch_size=self.args.batch_size, shuffle=False)
-            best_model_path = self.train(self.loss_func, optimizer, train_data_loader, val_data_loader)
+                val_data_loader = DataLoader(dataset=self.validation_set,
+                                             batch_size=self.args.batch_size,
+                                             shuffle=False)
+            best_model_path = self.train(self.loss_func, optimizer, train_data_loader,
+                                         val_data_loader)
         if self.args.do_predict:
-            test_data_loader = DataLoader(dataset=self.test_set, batch_size=self.args.batch_size, shuffle=False)
+            test_data_loader = DataLoader(dataset=self.test_set,
+                                          batch_size=self.args.batch_size,
+                                          shuffle=False)
             logger.info(f">> load best model: {best_model_path.split('/')[-1]}")
-            self.model.load_state_dict(torch.load(best_model_path, map_location=torch.device(self.args.device)))
+            self.model.load_state_dict(
+                torch.load(best_model_path, map_location=torch.device(self.args.device)))
             # self.model = torch.load(best_model_path, map_location=torch.device(self.args.device))
             test_pre, test_rec, test_f1 = self.evaluate(test_data_loader)
             content = f'test_pre: {test_pre:.4f}, test_rec: {test_rec:.4f}, test_f1: {test_f1:.4f}'
             logger.info(f'>> {content}')
             with open(os.path.join(self.args.output_dir, "absa_prediction.txt"), "w") as f:
                 f.write(content)
-        shutil.move(best_model_path, os.path.join(self.args.output_dir, best_model_path.split("/")[-1]))
+        shutil.move(best_model_path,
+                    os.path.join(self.args.output_dir,
+                                 best_model_path.split("/")[-1]))
+
 
 if __name__ == '__main__':
     args = parse_args()[0]
