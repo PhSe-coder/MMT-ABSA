46b923d41eb603ebac1b96ba9b322170265b3897
Phse-coder <2368732089@qq.com>
2022-11-25 18:56:50 +0800

    preprocess1

--- preprocess1.py
+++ preprocess1.py
@@ -31,26 +31,26 @@ alpha_sr = 0.05
 alpha_rd = 0
 alpha_ri = 0
 alpha_rs = 0
-# logger.info("split train set into train/validation dataset, save in %s", tmp_dst_1)
-# split(args.src, tmp_dst_1)
-# logger.info("copy test dataset into %s", tmp_dst_1)
-# for file in glob("./data/*.test.txt"):
-#     copy(file, tmp_dst_1)
+logger.info("split train set into train/validation dataset, save in %s", tmp_dst_1)
+split(args.src, tmp_dst_1)
+logger.info("copy test dataset into %s", tmp_dst_1)
+for file in glob("./data/*.test.txt"):
+    copy(file, tmp_dst_1)
 
-# makedirs(tmp_dst_2, exist_ok=True)
-# for file in listdir(tmp_dst_1):
-#     input_file = osp.join(tmp_dst_1, file)
-#     output_file = osp.join(tmp_dst_2, file)
-#     if "train" not in input_file:
-#         logger.info("copy %s dataset into %s", file, tmp_dst_2)
-#         copy(input_file, tmp_dst_2)
-#         continue
-#     logger.info("process dataset %s with pos,dep annotation", file)
-#     ann.run(
-#         ann.parser.parse_args([
-#             "--dataset", input_file, "--output-file", output_file, "--batch-size",
-#             str(ann_batch_size)
-#         ]))
+makedirs(tmp_dst_2, exist_ok=True)
+for file in listdir(tmp_dst_1):
+    input_file = osp.join(tmp_dst_1, file)
+    output_file = osp.join(tmp_dst_2, file)
+    if "train" not in input_file:
+        logger.info("copy %s dataset into %s", file, tmp_dst_2)
+        copy(input_file, tmp_dst_2)
+        continue
+    logger.info("process dataset %s with pos,dep annotation", file)
+    ann.run(
+        ann.parser.parse_args([
+            "--dataset", input_file, "--output-file", output_file, "--batch-size",
+            str(ann_batch_size)
+        ]))
 
 with open(pos_file, "w") as pos_writer, open(deprel_file, "w") as deprel_writer:
     pos_tag_dict = {}
@@ -71,8 +71,36 @@ with open(pos_file, "w") as pos_writer, open(deprel_file, "w") as deprel_writer:
                     deprel_tag_dict[f"B{deprel[1:]}"] = deprel_tag_dict.get(f"B{deprel[1:]}", 0) + 1
                     deprel_tag_dict[f"I{deprel[1:]}"] = deprel_tag_dict.get(f"I{deprel[1:]}",
                                                                             0) + len(res[1:])
-    pos_writer.writelines([tag + '\n' for tag, count in pos_tag_dict.items() if count >= 10])
-    deprel_writer.writelines([tag + '\n' for tag, count in deprel_tag_dict.items() if count >= 10])
+    pos_tag_num = sum(count for _, count in pos_tag_dict.items())
+    deprel_tag_num = sum(count for _, count in deprel_tag_dict.items())
+    thr = 0.9
+    sorted_pos_tag_items = sorted([(tag, num) for tag, num in pos_tag_dict.items()],
+                                  key=lambda item: item[1],
+                                  reverse=True)
+    for i in range(len(pos_tag_dict)):
+        if sum(count for _, count in sorted_pos_tag_items[:i]) / pos_tag_num > thr:
+            break
+    b_unk_count = sum(num for tag, num in sorted_pos_tag_items[i:] if tag.startswith("B"))
+    i_unk_count = sum(num for tag, num in sorted_pos_tag_items[i:] if tag.startswith("I"))
+    valid_pos_list = sorted_pos_tag_items[:i]
+    valid_pos_list.append(('B-[UNK]', b_unk_count))
+    valid_pos_list.append(('I-[UNK]', i_unk_count))
+    valid_pos_list = sorted(valid_pos_list, key=lambda item: item[1], reverse=True)
+    pos_writer.writelines([f"{tag} {count/pos_tag_num}\n" for tag, count in valid_pos_list])
+    sorted_deprel_tag_items = sorted([(tag, num) for tag, num in deprel_tag_dict.items()],
+                                  key=lambda item: item[1],
+                                  reverse=True)
+    for i in range(len(deprel_tag_dict)):
+        if sum(count for _, count in sorted_deprel_tag_items[:i]) / deprel_tag_num > thr:
+            break
+    b_unk_count = sum(num for tag, num in sorted_deprel_tag_items[i:] if tag.startswith("B"))
+    i_unk_count = sum(num for tag, num in sorted_deprel_tag_items[i:] if tag.startswith("I"))
+    valid_deprel_list = sorted_deprel_tag_items[:i]
+    valid_deprel_list.append(('B-[UNK]', b_unk_count))
+    valid_deprel_list.append(('I-[UNK]', i_unk_count))
+    valid_deprel_list = sorted(valid_deprel_list, key=lambda item: item[1], reverse=True)
+    deprel_writer.writelines(
+        [f"{tag} {count/deprel_tag_num}\n" for tag, count in valid_deprel_list])
 
 makedirs(tmp_dst_3, exist_ok=True)
 for file in listdir(tmp_dst_2):
