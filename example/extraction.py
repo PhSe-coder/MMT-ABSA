6e4521b6beafafe6340b1a1895f57b1451d6995e
Phse-coder <2368732089@qq.com>
2022-12-07 02:01:35 +0800

    example

--- example/extraction.py
+++ example/extraction.py
@@ -7,6 +7,8 @@ from random import randint
 import time
 import os
 from typing import List
+from urllib3.exceptions import MaxRetryError
+from requests.exceptions import ProxyError, JSONDecodeError
 import numpy as np
 import fasttext.util
 import fasttext
@@ -15,13 +17,12 @@ from tqdm import tqdm
 
 from utils.tag_utils import Annotate, get_base_classes_of_item
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 tagging_schemas = ['t', 'bio']
 ready = False
-
 parser = ArgumentParser(description='Extract and replace entities fot a dataset')
 parser.add_argument("--sep", default="***", type=str, help="data item seperator")
 parser.add_argument("--dataset", required=True, help="Dataset to be extracted")
@@ -29,7 +30,9 @@ parser.add_argument("--output-file",
                     required=True,
                     help="Path where the extraction result should to be saved")
 parser.add_argument("--batch-size", type=int, default=32, help="extraction batch size")
-parser.add_argument("--entity-path", help="path of the wikidata entity file")
+parser.add_argument("--entity-path",
+                    default="./wikidata5m_entity.txt",
+                    help="path of the wikidata entity file")
 parser.add_argument("--mean-vec", help="mean vector numpy file path")
 parser.add_argument("--schema",
                     type=str,
@@ -56,8 +59,18 @@ class Producer(threading.Thread):
         fasttext.util.download_model('en', if_exists='ignore')  # English
         self.model = fasttext.load_model('cc.en.300.bin')
         self.mean_vec = np.load(args.mean_vec)
+        self.entity_map = {}
+        total = sum(1 for _ in open(self.entity_path, "rb"))
+        logger.info(f"reading wikidata entity file {self.entity_path}")
+        with open(self.entity_path, encoding='utf-8') as f:
+            for line in tqdm(f, total=total):
+                e_id, e_str = line.strip().split("\t", maxsplit=1)
+                [self.entity_map.__setitem__(e, e_id) for e in e_str.split("\t")]
+        pass
 
     def process(self, index: int, sentence: str):
+        # start = time.time()
+        # print(index, "start", time.time() - start)
         try:
             res_dict = Annotate(sentence, theta=0.05)
             if not res_dict:
@@ -66,8 +79,8 @@ class Producer(threading.Thread):
             if len(res_dict) == 1:
                 result = res_dict
             else:
-                begins = list(zip(*res_dict.keys()))[0]
-                ends = list(zip(*res_dict.keys()))[1]
+                begins = tuple(begin for begin, _ in res_dict.keys())
+                ends = tuple(end for _, end in res_dict.keys())
                 for i in range(len(begins) - 1):
                     assert begins[i] <= begins[i + 1]
                     # preserve the longest entity
@@ -75,8 +88,8 @@ class Producer(threading.Thread):
                         res_dict.pop((begins[i], ends[i]), None)
                 # merge the entity with continuous index
                 result = {}
-                begins = list(zip(*res_dict.keys()))[0]
-                ends = list(zip(*res_dict.keys()))[1]
+                begins = tuple(begin for begin, _ in res_dict.keys())
+                ends = tuple(end for _, end in res_dict.keys())
                 ent = {}
                 for i in range(len(begins) - 1):
                     if ends[i] + 1 == begins[i + 1]:
@@ -101,8 +114,8 @@ class Producer(threading.Thread):
                 if not result:
                     result = res_dict
                 # remove the overlap entites
-                begins = list(zip(*result.keys()))[0]
-                ends = list(zip(*result.keys()))[1]
+                begins = tuple(begin for begin, _ in result.keys())
+                ends = tuple(end for _, end in result.keys())
                 for i in range(len(begins) - 1):
                     if ends[i] + 1 > begins[i + 1]:
                         len1 = ends[i] - begins[i]
@@ -115,6 +128,7 @@ class Producer(threading.Thread):
                             idx = randint(0, 1)
                             result.pop((begins[i + idx], ends[i + idx]), None)
             res_result = {}
+            # print(index, "before iter", time.time() - start)
             for k in result:
                 baseclass_items = {}
                 # for the merged entity, preserve the most similar entity
@@ -134,40 +148,41 @@ class Producer(threading.Thread):
                     cosine_similarity(
                         self.mean_vec.reshape(1, -1),
                         np.array(self.model.get_word_vector(entity_title.lower())).reshape(1, -1)))
-                time.sleep(1)
-                for item in get_base_classes_of_item(
-                        self.get_entity_id(entity_title), self.args.entity_save_path
-                ):  # replace original item if the baseclass item can increase the domain similarity
+                # print(index, "base classes", time.time() - start)
+                qid = self.entity_map.get(entity_title, None)
+                # replace original item if the baseclass item can increase the domain similarity
+                time.sleep(randint(0, 5))
+                for item in get_base_classes_of_item(qid, self.args.entity_save_path):
                     _sim = float(
                         cosine_similarity(
                             self.mean_vec.reshape(1, -1),
                             np.array(self.model.get_word_vector(item.lower())).reshape(1, -1)))
                     if _sim > sim:
                         baseclass_items[item] = _sim
-                items = tuple(
-                    zip(*sorted(baseclass_items.items(), key=lambda item: item[1], reverse=True)))
-                res_result[k] = (*result[k], items[0] if items else ())
+                items = tuple(item for item, _ in sorted(
+                    baseclass_items.items(), key=lambda item: item[1], reverse=True))
+                res_result[k] = (*result[k], items)
+                # print(index, "one iter end", time.time() - start)
+        except (MaxRetryError, ProxyError, JSONDecodeError) as e:
+            logger.warning(e.args)
+            time.sleep(1)
+            return self.process(index, sentence)
         except Exception as e:
             raise e
+        # print(index, "final", time.time() - start)
         return {index: res_result}
-
-    def get_entity_id(self, entity_title: str):
-        with open(self.entity_path, encoding='utf-8') as f:
-            for line in f:
-                if f'\t{entity_title}\t' in line:
-                    return line.split("\t", maxsplit=1)[0]
-            return None
 
     def put(self, text_list: List[str], rest_list: List[str]):
         results = {}
-        with ThreadPoolExecutor(max_workers=128) as t:
+        with ThreadPoolExecutor(max_workers=16) as t:
             for future in tqdm(as_completed(
                 [t.submit(self.process, idx, sentence) for idx, sentence in enumerate(text_list)]),
-                               total=self.args.batch_size,
+                               total=len(text_list),
                                disable=False):  # disable the bar
                 if future.exception():
                     future.set_result({})
-                    logger.warn(future.exception().args)
+                    logger.warning(future.exception().args)
+                    exit(-1)
                 results.update(future.result())
         res = [result for _, result in sorted(results.items(), key=lambda item: item[0])]
         for i, text in enumerate(text_list):
@@ -214,8 +229,30 @@ class Consumer(threading.Thread):
             while True:
                 try:
                     text, labels, res_dict = self.data.get(timeout=5)
-                    tokens = text.split()
-                    labels = labels.split()
+                    tokens = text.strip().split()
+                    labels = labels.strip().split()
+                    offset = 0
+                    tar_senti = []
+                    for k, v in res_dict.items():
+                        # skip if no baseclass item exists for the entity
+                        if v[-1] == ():
+                            continue
+                        idx_tuple = tuple(text[:idx].count(" ") + offset for idx in k)
+                        assert len(idx_tuple) == 2
+                        # skip if token overlapping exists between extracted entity and aspect terms
+                        sentis = set(
+                            [labels[i][-3:] for i in range(idx_tuple[0], idx_tuple[1] + 1)])
+                        if len(sentis) != 1:
+                            continue
+                        senti = "T-" + sentis.pop()
+                        original_span = idx_tuple[1] + 1 - idx_tuple[0]
+                        replaced_item = v[-1][0].split(" ")
+                        replaced_span = len(replaced_item)
+                        offset += (replaced_span - original_span)
+                        tokens = tokens[:idx_tuple[0]] + replaced_item + tokens[idx_tuple[1] + 1:]
+                        labels = labels[:idx_tuple[0]] + [senti
+                                                          ] * replaced_span + labels[idx_tuple[1] +
+                                                                                     1:]
                     tar_dict = {}
                     idx_list = []
                     tag = ''
@@ -227,24 +264,10 @@ class Consumer(threading.Thread):
                         if idx_list:
                             tar_dict[(idx_list[0], idx_list[-1])] = tag[-3:]
                             idx_list.clear()
-                    offset = 0
-                    for k, v in res_dict.items():
-                        # skip if no baseclass item exists for the entity
-                        if v[-1] == ():
-                            continue
-                        idx_tuple = tuple(text[:idx].count(" ") + offset for idx in k)
-                        assert len(idx_tuple) == 2
-                        # skip if token overlapping exists between extracted entity and aspect terms
-                        if len(set([labels[i][-3:]
-                                    for i in range(idx_tuple[0], idx_tuple[1] + 1)])) != 1:
-                            continue
-                        original_span = idx_tuple[1] + 1 - idx_tuple[0]
-                        replaced_item = v[-1][0].split(" ")
-                        replaced_span = len(replaced_item)  # random select a baseclass item
-                        offset += (replaced_span - original_span)
-                        tokens = tokens[:idx_tuple[0]] + replaced_item + tokens[idx_tuple[1] + 1:]
-                    tar_senti = sorted(tar_dict.items(), key=lambda d: d[0][0])
-                    tar_senti = [((k[0] + offset, k[1] + offset), v) for k, v in tar_senti]
+                    if idx_list:
+                        tar_dict[(idx_list[0], idx_list[-1])] = tag[-3:]
+                        idx_list.clear()
+                    tar_senti = list(tar_dict.items())
                     labels: List[str] = []
                     cur = 0
                     i = 0
@@ -270,7 +293,7 @@ class Consumer(threading.Thread):
                                 if len(tar_senti[cur]
                                        [0]) == 1 or tar_senti[cur][0][0] == tar_senti[cur][0][1]:
                                     cur += 1
-                            elif i > tar_senti[cur][0][0] and i < tar_senti[cur][0][1]:
+                            elif i > tar_senti[cur][0][0] and i <= tar_senti[cur][0][1]:
                                 if self.args.schema == 't':
                                     tag = "T-" + tar_senti[cur][1]
                                     labels.append(tag)
@@ -281,25 +304,15 @@ class Consumer(threading.Thread):
                                     raise ValueError(
                                         "tagging schema must in {}. Got {} instead.".format(
                                             tagging_schemas, self.args.schema))
-                            elif i == tar_senti[cur][0][1]:
-                                if self.args.schema == 't':
-                                    tag = "T-" + tar_senti[cur][1]
-                                    labels.append(tag)
-                                elif self.args.schema == 'bio':
-                                    tag = "I-" + tar_senti[cur][1]
-                                    labels.append(tag)
-                                else:
-                                    raise ValueError(
-                                        "tagging schema must in {}. Got {} instead.".format(
-                                            tagging_schemas, self.args.schema))
-                                cur += 1
+                                if i == tar_senti[cur][0][1]:
+                                    cur += 1
                         except IndexError as e:
                             # process overlapped aspect terms
                             cur += 1
                             i -= 1
                             logger.debug(f"{text} has OVERLAPPED aspect terms")
                         i += 1
-                    assert len(tokens) == len(labels)
+                    assert len(tokens) == len(labels), f"{tokens} {labels}"
                     f.write(f"{text}{self.sep}{' '.join(labels)}\n")
                 except Empty:
                     if ready:
@@ -322,7 +335,7 @@ def run(args):
 if __name__ == '__main__':
     run(
         parser.parse_args([
-            "--dataset", "./data/rest.train.txt", "--output-file",
-            "processed/ent_tmp/rest.train.txt", "--batch-size", "512", "--entity-path",
-            "./wikidata5m_entity.txt", "--mean-vec", "./processed/rest_mean_vec.npy"
+            "--dataset", "./data/device.train.txt", "--output-file",
+            "processed/ent_tmp/device.train.txt", "--batch-size", "512", "--mean-vec",
+            "./processed/vectors/device_mean_vec.npy"
         ]))
