ab8e8a4391357c4f761ecfb89ce05748a793213d
Phse-coder <2368732089@qq.com>
2022-12-06 15:02:14 +0800

    scripts

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:$(pwd)
-
+wget -O /tmp/wikidata5m_alias.tar.gz https://www.dropbox.com/s/lnbhc8yuhit4wm5/wikidata5m_alias.tar.gz?dl=1
+tar -zxvf /tmp/wikidata5m_alias.tar.gz -C ./
 python ./preprocess/ent_link.py --src ./data --dest ./processed/ent_tmp
 python ./preprocess/split.py --src ./processed/ent_tmp --dest ./processed/tmp
 python ./preprocess/ann.py --src ./processed/tmp --dest ./processed/ann_tmp
