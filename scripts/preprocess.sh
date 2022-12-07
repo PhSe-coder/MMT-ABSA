ce6fe06bde3aa24153983336858e8baba1bca285
Phse-coder <2368732089@qq.com>
2022-12-07 18:35:06 +0800

    scripts

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+export HTTP_PROXY=http://127.0.0.1:7890
+export HTTPS_PROXY=http://127.0.0.1:7890
+export NO_PROXY=tagme.d4science.org
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 FILE=./wikidata5m_entity.txt
 if [[ ! -f "$FILE" ]]; then
@@ -6,9 +9,11 @@ if [[ ! -f "$FILE" ]]; then
     tar -zxvf /tmp/wikidata5m_alias.tar.gz -C ./
 fi
 python ./preprocess/get_entity.py --src ./data
-python ./preprocess/ent_replace.py --src ./data --dest ./processed/ent_tmp
+python ./preprocess/ent_replace.py --src ./data --dest ./processed/ent_tmp --max-workers 128
+sleep 5s
 python ./preprocess/split.py --src ./processed/ent_tmp --dest ./processed/tmp
 python ./preprocess/ann.py --src ./processed/tmp --dest ./processed/ann_tmp
 python ./preprocess/dp.py --src ./processed/ann_tmp --dest ./processed/dp_tmp
+sleep 5s
 python ./preprocess/augment.py --src ./processed/dp_tmp --dest ./processed/ag_tmp --alpha-sr 0.3 --alpha-ri 0.1 --alpha-rs 0.1
 python ./preprocess/final.py --src ./processed/ag_tmp --dest ./processed/dataset
