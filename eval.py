3a05f745056fc90a6aa1f13d3b69b65e8b07d1b1
Phse-coder <2368732089@qq.com>
2023-01-18 17:20:36 +0800

    evaluation metrics

--- eval.py
+++ eval.py
@@ -1,21 +1,42 @@
 from typing import List, Tuple
 
 
-def absa_evaluate(pred_Y: List[List[str]], gold_Y: List[List[str]]):
-    """evaluate function for end2end aspect based sentiment analysis, with labels: {B,I}-{POS, NEG, NEU} and O
+# def absa_evaluate(pred_Y: List[List[str]], gold_Y: List[List[str]]):
+#     """evaluate function for end2end aspect based sentiment analysis, with labels: {B,I}-{POS, NEG, NEU} and O
 
-    Parameters
-    ----------
-    pred_Y : List[List[str]]
-        predicted tags
-    gold_Y : List[List[str]]
-        gold standard tags (i.e., post-processed labels)
+#     Parameters
+#     ----------
+#     pred_Y : List[List[str]]
+#         predicted tags
+#     gold_Y : List[List[str]]
+#         gold standard tags (i.e., post-processed labels)
 
-    Returns
-    -------
-    Tuple[float, float, str]
-        precision, recall, micro f1
-    """
+#     Returns
+#     -------
+#     Tuple[float, float, str]
+#         precision, recall, micro f1
+#     """
+#     assert len(gold_Y) == len(pred_Y)
+#     TP, FN, FP = 0, 0, 0
+#     for i in range(len(gold_Y)):
+#         gold = gold_Y[i]
+#         pred = pred_Y[i]
+#         assert len(gold) == len(pred)
+#         gold_aspects = tag2aspect_sentiment(gold)
+#         pred_aspects = tag2aspect_sentiment(pred)
+#         hits_num = match(pred=pred_aspects, gold=gold_aspects)
+#         TP += hits_num
+#         FP += (len(pred_aspects) - hits_num)
+#         FN += (len(gold_aspects) - hits_num)
+
+#     pre = float(TP) / float(TP + FP + 0.00001)
+#     rec = float(TP) / float(TP + FN + 0.00001)
+#     f1 = 2 * pre * rec / (pre + rec + 0.00001)
+
+#     return pre, rec, f1
+
+
+def absa_evaluate(pred_Y: List[List[Tuple]], gold_Y: List[List[Tuple]]):
     assert len(gold_Y) == len(pred_Y)
     TP, FN, FP = 0, 0, 0
     for i in range(len(gold_Y)):
@@ -91,7 +112,7 @@ def tag2aspect_sentiment(ts_tag_sequence: List[str]) -> List[Tuple[int, int, str
                 if start != -1:
                     sentiments.append(pos)
     if start != -1:
-        ts_sequence.append((start, index, sentiments[0]))
+        ts_sequence.append((sentiments[0], (start, index)))
     return ts_sequence
 
 
