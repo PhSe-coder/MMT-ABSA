40260bed183277ff361ef52ddb7223d2cfcc3e5d
Phse-coder <2368732089@qq.com>
2023-01-18 18:13:59 +0800

    utils

--- utils/tag_utils.py
+++ utils/tag_utils.py
@@ -1,6 +1,6 @@
 import logging
 import os
-from typing import List, Tuple, Union
+from typing import List, Tuple, Union, Set
 import tagme
 import pickle
 from qwikidata.sparql import return_sparql_query_results
@@ -10,6 +10,17 @@ tagme.GCUBE_TOKEN = "58cf013e-71b9-4d8d-a7c1-396f5e842bec-843339462"
 # logger = logging.getLogger(__name__)
 # logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
 
+TypedStringSpan = Tuple[str, Tuple[int, int]]
+
+
+class InvalidTagSequence(Exception):
+
+    def __init__(self, tag_sequence=None):
+        super().__init__()
+        self.tag_sequence = tag_sequence
+
+    def __str__(self):
+        return " ".join(self.tag_sequence)
 
 def ot2bio_absa(ts_tag_sequence: List[str]):
     new_ts_sequence: List[str] = []
@@ -96,3 +107,76 @@ SELECT ?pLabel WHERE {{
         with open(path, "wb") as f:
             pickle.dump(ret, f)
     return ret
+
+
+def bio_tags_to_spans(tag_sequence: List[str],
+                      classes_to_ignore: List[str] = None) -> List[TypedStringSpan]:
+    """
+    Given a sequence corresponding to BIO tags, extracts spans.
+    Spans are inclusive and can be of zero length, representing a single word span.
+    Ill-formed spans are also included (i.e those which do not start with a "B-LABEL"),
+    as otherwise it is possible to get a perfect precision score whilst still predicting
+    ill-formed spans in addition to the correct spans. This function works properly when
+    the spans are unlabeled (i.e., your labels are simply "B", "I", and "O").
+
+    # Parameters
+
+    tag_sequence : `List[str]`, required.
+        The integer class labels for a sequence.
+    classes_to_ignore : `List[str]`, optional (default = `None`).
+        A list of string class labels `excluding` the bio tag
+        which should be ignored when extracting spans.
+
+    # Returns
+
+    spans : `List[TypedStringSpan]`
+        The typed, extracted spans from the sequence, in the format (label, (span_start, span_end)).
+        Note that the label `does not` contain any BIO tag prefixes.
+    """
+    classes_to_ignore = classes_to_ignore or []
+    spans: Set[Tuple[str, Tuple[int, int]]] = set()
+    span_start = 0
+    span_end = 0
+    active_conll_tag = None
+    for index, string_tag in enumerate(tag_sequence):
+        # Actual BIO tag.
+        bio_tag = string_tag[0]
+        if bio_tag not in ["B", "I", "O"]:
+            raise InvalidTagSequence(tag_sequence)
+        conll_tag = string_tag[2:]
+        if bio_tag == "O" or conll_tag in classes_to_ignore:
+            # The span has ended.
+            if active_conll_tag is not None:
+                spans.add((active_conll_tag, (span_start, span_end)))
+            active_conll_tag = None
+            # We don't care about tags we are
+            # told to ignore, so we do nothing.
+            continue
+        elif bio_tag == "B":
+            # We are entering a new span; reset indices
+            # and active tag to new span.
+            if active_conll_tag is not None:
+                spans.add((active_conll_tag, (span_start, span_end)))
+            active_conll_tag = conll_tag
+            span_start = index
+            span_end = index
+        elif bio_tag == "I" and conll_tag == active_conll_tag:
+            # We're inside a span.
+            span_end += 1
+        else:
+            # This is the case the bio label is an "I", but either:
+            # 1) the span hasn't started - i.e. an ill formed span.
+            # 2) The span is an I tag for a different conll annotation.
+            # We'll process the previous span if it exists, but also
+            # include this span. This is important, because otherwise,
+            # a model may get a perfect F1 score whilst still including
+            # false positive ill-formed spans.
+            if active_conll_tag is not None:
+                spans.add((active_conll_tag, (span_start, span_end)))
+            active_conll_tag = conll_tag
+            span_start = index
+            span_end = index
+    # Last token might have been a part of a valid span.
+    if active_conll_tag is not None:
+        spans.add((active_conll_tag, (span_start, span_end)))
+    return list(spans)
