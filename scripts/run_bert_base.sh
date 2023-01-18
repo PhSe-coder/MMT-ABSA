da9468e37b5787b677638e959c37e479ff3b407f
Phse-coder <2368732089@qq.com>
2023-01-18 17:55:06 +0800

    scripts

--- scripts/run_bert_base.sh
+++ scripts/run_bert_base.sh
@@ -2,43 +2,45 @@
 domains=('rest' 'service' 'laptop' 'device')
 
 export CUDA_VISIBLE_DEVICES=0
+export CUDA_LAUNCH_BLOCKING=1
 export TRANSFORMERS_OFFLINE=0
+export DGLBACKEND=pytorch
 output='./out/bert_base/'
-train_dir='./data'
+train_dir='./processed/dataset/'
 val_dir='./processed/dataset'
 test_dir='./processed/dataset'
-for tar_domain in ${domains[@]};
+for tgt_domain in ${domains[@]};
 do
     for src_domain in  ${domains[@]};
     do
-        if [ $src_domain != $tar_domain ];
+        if [ $src_domain != $tgt_domain ];
         then
-            if [ $src_domain == 'laptop' -a  $tar_domain == 'device' ];
+            if [ $src_domain == 'laptop' -a  $tgt_domain == 'device' ];
             then
                 continue
             fi
-            if [ $src_domain == 'device' -a  $tar_domain == 'laptop' ];
+            if [ $src_domain == 'device' -a  $tgt_domain == 'laptop' ];
             then
                 continue
             fi
-	        python run.py \
-                -m torch.distributed.launch \
-                --nproc_per_node=1 \
-                --local_rank 0 \
-                --model_name "bert" \
-                --output_dir "${output}${src_domain}-${tar_domain}"  \
-                --train_file "${train_dir}/${src_domain}.train.txt" \
-                --test_file "${test_dir}/${tar_domain}.test.txt" \
-                --validation_file "${val_dir}/${tar_domain}.validation.txt" \
-                --do_train \
-                --do_predict \
-                --do_eval \
-                --device "cuda:0" \
-                --optimizer "rmsprop" \
-                --lr "1e-3" \
-                --bert_lr "3e-5" \
-                --batch_size 16 \
-                --num_train_epochs 3
+	        python -m torch.distributed.launch --nproc_per_node=1 --master_addr=127.0.0.1 --master_port=12345 \
+            run.py \
+            --model_name "bert" \
+            --output_dir "${output}/${src_domain}-${tgt_domain}"  \
+            --train_file "${train_dir}/${src_domain}.train.txt" "${train_dir}/${tgt_domain}.train.txt" \
+            --test_file "${test_dir}/${tgt_domain}.test.txt" \
+            --do_train \
+            --do_predict \
+            --optimizer "adamW" \
+            --l2reg 0.1 \
+            --warmup 0.3 \
+            --lr "3e-5" \
+            --bert_lr "3e-5" \
+            --batch_size 16 \
+            --num_workers 16 \
+            --num_train_epochs 3 \
+            --seed 1 \
+            --initializer xavier_uniform_
         fi
     done
 done
