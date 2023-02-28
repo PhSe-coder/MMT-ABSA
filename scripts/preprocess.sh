162765558661d01acc6ad16a7547985dfefbc94b
Phse-coder <2368732089@qq.com>
2023-02-28 20:24:06 +0800

    scripts

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -5,11 +5,15 @@
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 # python ./preprocess/get_entity.py --src ./data
 # # python ./preprocess/ent_replace.py --src ./data --dest ./processed/ent_tmp --max-workers 128
-python ./preprocess/split.py --src ./data --dest ./processed/tmp --split-rate 0.7
+# python ./preprocess/split.py --src ./data --dest ./processed/tmp --split-rate 0.7
 # python ./preprocess/ann.py --src ./data --dest ./processed/ann_tmp
 # python ./preprocess/ent_link.py --src ./processed/ann_tmp --dest ./processed/ent_tmp
 # python ./preprocess/dp.py --src ./processed/ann_tmp --dest ./processed/dp_tmp
-python ./preprocess/augment.py --src ./processed/tmp  --dest ./processed/ag_tmp --alpha-sr 0.3 --num 4
-python ./preprocess/ann.py --src ./processed/ag_tmp --dest ./processed/dataset
+python ./preprocess/augment.py --src ./processed/tmp --dest ./processed/dataset --alpha-sr 0.3 --num 3 --seed 1
+python ./preprocess/augment.py --src ./processed/tmp --dest ./processed/cont_dataset --alpha-sr 0.3 --num 3 --seed 42
+# python ./preprocess/cont.py --src ./processed/ag_tmp --dest ./processed/cont_tmp
+# python ./preprocess/ann.py --src ./processed/ag_tmp --dest ./processed/dataset
+# python ./preprocess/ann.py --src ./processed/cont_tmp --dest ./processed/cont_dataset
+
 # python ./preprocess/ent_link.py --src ./processed/ag_tmp --dest ./processed/dataset
 # python ./preprocess/final.py --src ./processed/ag_tmp --dest ./processed/dataset
