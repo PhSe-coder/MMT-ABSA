1b5d05c74ae479b33fc6c44095da8e9cd9e56ce7
Phse-coder <2368732089@qq.com>
2023-03-10 17:01:17 +0800

    scripts

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# export HTTP_PROXY=http://127.0.0.1:7890
-# export HTTPS_PROXY=http://127.0.0.1:7890
+export HTTP_PROXY=http://127.0.0.1:7890
+export HTTPS_PROXY=http://127.0.0.1:7890
 # export NO_PROXY=tagme.d4science.org
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 # python ./preprocess/get_entity.py --src ./data
@@ -9,8 +9,8 @@ export PYTHONPATH=$PYTHONPATH:$(pwd)
 # python ./preprocess/ann.py --src ./data --dest ./processed/ann_tmp
 # python ./preprocess/ent_link.py --src ./processed/ann_tmp --dest ./processed/ent_tmp
 # python ./preprocess/dp.py --src ./processed/ann_tmp --dest ./processed/dp_tmp
-python ./preprocess/augment.py --src ./processed/tmp --dest ./processed/dataset --alpha-sr 0.3 --num 3 --seed 1
-python ./preprocess/augment.py --src ./processed/tmp --dest ./processed/cont_dataset --alpha-sr 0.3 --num 3 --seed 42
+# python ./preprocess/augment.py --src ./processed/tmp --dest ./processed/dataset --alpha-sr 0.3 --num 3 --seed 1
+python ./preprocess/augment.py --src ./processed/tmp --dest ./processed/cont --alpha-sr 0.3 --num 1 --seed 42
 # python ./preprocess/cont.py --src ./processed/ag_tmp --dest ./processed/cont_tmp
 # python ./preprocess/ann.py --src ./processed/ag_tmp --dest ./processed/dataset
 # python ./preprocess/ann.py --src ./processed/cont_tmp --dest ./processed/cont_dataset
